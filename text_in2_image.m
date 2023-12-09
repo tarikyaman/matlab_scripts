1e8b807d1ddbeab340e0ceebe88e0fae786338ca
tarikyaman <153305934+tarikyaman@users.noreply.github.com>
2023-12-09 23:12:13 +0300

    Add files via upload

---
+++ text_in2_image.m
@@ -0,0 +1,12 @@
+clc;
+clear;
+
+
+t=0.0000625;
+imgfiles1 = dir('*.jpeg');
+for v = 1:length(imgfiles1)
+    IM = imread(imgfiles1(v).name);
+    IM = insertText(IM,[0 0],sprintf('TIME STEP : %4d\nTime : %8.7f [S]\nPinion M4.5 Z16 240 RPM\nWhell M4.5 Z24 160 RPM\n0.09 Deg/TimeStep\n0.99/1 Scaled',v-1,(v-1)*t),'AnchorPoint','LeftTop','BoxColor','r',BoxOpacity=1,FontSize=50);
+    imwrite(IM,imgfiles1(v).name);
+    fprintf('%s is Processing\n.',string(imgfiles1(v).name));
+end
